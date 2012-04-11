c202bb1861bd4394594f1e06283fb9dc851e9bc1
Katie Bane <foxx9115@cox.net>
2012-04-10 22:05:39 -0500

    almost done with homepage aand information page food and ent are next

--- urls.py
+++ urls.py
@@ -7,7 +7,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', 'pullover.locater.views.home', name='home'), 
     url(r'^town/(?P<town_id>\d+)/$', 'pullover.locater.views.detail'),
-    # url(r'^town/info/(?P<info_id>\d+)/$', 'pullover.information.views.info'),
+    # url(r'^town/<food>/(?P<food_id>\d+)/$', 'pullover.information.views.food'),
+    # url(r'^town/<ent>/(?P<ent_id>\d+)/$', 'pullover.information.views.ent'),
 
     # url(r'^$', 'pullover.views.home', name='home'),
     # url(r'^pullover/', include('pullover.foo.urls')),
